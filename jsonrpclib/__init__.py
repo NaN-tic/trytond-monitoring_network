16b472e134fbe8e1678b4a7bb913751bf2f98e2f
Albert Cervera i Areny <albert@nan-tic.com>
2015-04-25 01:53:56 +0200

    In jsonrpclib do not add jsonrpclib to imort modules.

--- jsonrpclib/__init__.py
+++ jsonrpclib/__init__.py
@@ -1,6 +1,6 @@
-from jsonrpclib.config import Config
+from .config import Config
 config = Config.instance()
-from jsonrpclib.history import History
+from .history import History
 history = History.instance()
-from jsonrpclib.jsonrpc import Server, MultiCall, Fault
-from jsonrpclib.jsonrpc import ProtocolError, loads, dumps
+from .jsonrpc import Server, MultiCall, Fault
+from .jsonrpc import ProtocolError, loads, dumps
